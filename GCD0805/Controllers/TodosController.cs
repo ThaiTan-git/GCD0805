8436f5b1d0cbf6673d801286694e61b793f5096d
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2021-10-31 15:11:06 +0700

    Create  done

--- GCD0805/Controllers/TodosController.cs
+++ GCD0805/Controllers/TodosController.cs
@@ -1,28 +1,52 @@
 ﻿using GCD0805.Models;
-using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace GCD0805.Controllers
 {
     public class TodosController : Controller
     {
+        private ApplicationDbContext _context;
+        public TodosController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Todos
+        [HttpGet]
         public ActionResult Index()
         {
-            Todo todo = new Todo()
+            /*Todo todo = new Todo()
             {
                 Id = 1,
                 Description = "On air",
                 DueDate = new DateTime(2021, 10, 30)
-            };
-            List<Todo> todos = new List<Todo>()
+            };*/
+            /*List<Todo> todos = new List<Todo>()
             {
                 new Todo(){ Id = 1, Description = "On air", DueDate = new DateTime(2021, 10, 30) },
                 new Todo(){ Id = 2, Description = "On air 2", DueDate = new DateTime(2021, 10, 31) },
                 new Todo(){ Id = 3, Description = "On air 3", DueDate = new DateTime(2021, 11, 1)}
-        };
+        };*/
+            var todos = _context.Todos.ToList();
             return View(todos);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Todo todo)
+        {
+            var newTodo = new Todo()
+            {
+                Description = todo.Description,
+                DueDate = todo.DueDate
+            };
+
+            _context.Todos.Add(newTodo);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Todos");
         }
     }
 }
