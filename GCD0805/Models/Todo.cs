cf6a705bb8218de7a8894ff348c85adc7cde1fc9
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2021-10-31 16:33:37 +0700

    Requirement done

--- GCD0805/Models/Todo.cs
+++ GCD0805/Models/Todo.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GCD0805.Models
 {
     public class Todo
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Description { get; set; }
+        [Required]
         public DateTime DueDate { get; set; }
     }
 }
