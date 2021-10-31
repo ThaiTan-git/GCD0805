e647fae243eb45290513b380d821b803eeed2bf7
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2021-10-31 12:20:31 +0700

    Version 1.1

--- GCD0805/Controllers/TodosController.cs
+++ GCD0805/Controllers/TodosController.cs
@@ -1,8 +1,6 @@
 ﻿using GCD0805.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GCD0805.Controllers
@@ -18,7 +16,13 @@ namespace GCD0805.Controllers
                 Description = "On air",
                 DueDate = new DateTime(2021, 10, 30)
             };
-            return View(todo);
+            List<Todo> todos = new List<Todo>()
+            {
+                new Todo(){ Id = 1, Description = "On air", DueDate = new DateTime(2021, 10, 30) },
+                new Todo(){ Id = 2, Description = "On air 2", DueDate = new DateTime(2021, 10, 31) },
+                new Todo(){ Id = 3, Description = "On air 3", DueDate = new DateTime(2021, 11, 1)}
+        };
+            return View(todos);
         }
     }
 }
