1f522709a9d45ce2b82770e30dbae106716a6a67
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2021-11-02 03:26:55 +0700

    Begin to create team

---
+++ GCD0805/Controllers/TeamsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace GCD0805.Controllers
+{
+    public class TeamsController : Controller
+    {
+        // GET: Teams
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
